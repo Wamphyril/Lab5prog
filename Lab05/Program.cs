d17b746d67f91dbe8636bfedf5393c812035067a
Wamphyril <66071879+Wamphyril@users.noreply.github.com>
2020-05-29 00:07:13 +0300

    Добавьте файлы проекта.

---
+++ Lab05/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("5ая Лабораторная работа");
+            Task1 t1 = new Task1();
+            t1.Choose();
+        }
+    }
+}
